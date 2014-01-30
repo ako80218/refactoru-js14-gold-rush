a403c69ba0f9a1d48a665ada9af15e9310c229a4
ako80218 <andrew@andrewotto.com>
2014-01-30 13:51:11 -0700

    users can click to add pins and click pins to remove them

--- main.js
+++ main.js
@@ -1,45 +1,34 @@
 $(document).ready(function(){
 	var pinImagePath = 'images/pin.png';
-	//test coordinates for boudaries
-	//gets the  X,Y coordinates of the click on 
-	// img#map and returns coordinates in an object with 
+	//gets the  X,Y coordinates of the click on img#map and returns coordinates in an object with 
 	 var getCoordinates = function(e){
 	 	var clickXY = {};
-	 	//-8 accounts for margin on body tag (normalize.css)
-	 	clickXY.x =e.clientX-8;
-	 	clickXY.y=e.clientY-8;
-	 	// console.log(clickXY);
+	 	// -21 on x and -33on y account for pin image see lines 25,26
+	 	var containerOffset= $('.container').offset();
+	 	clickXY.x =e.clientX - containerOffset.left -21;
+	 	clickXY.y=e.clientY - containerOffset.top - 35;
 	 	return clickXY;
 	 };
-	 //takes an object as the first parameter and returns creates a dom element based on the input object
+	 //takes an object as the first parameter and returns a dom element based on the input object
 	 var makePinElement = function(obj){
 	 	var pinElement = $('<img>');
 	 	var xValue = (obj.x.toString()) + 'px';
 	 	var yValue = (obj.y.toString()) + 'px';
-	 	// console.log('X: ' + xValue);
-	 	// console.log('Y: ' + yValue);
-
 	 	pinElement.attr('src', pinImagePath).addClass('pin').css({
 	 		position:'absolute',
 	 		top: yValue,
 	 		left: xValue,
-	 		
+	 		width: '40px',
+	 		height: '40px'
 	 	});
-	 	console.log(pinElement);
 	 	return pinElement;
-
-
-
 	 };
-
- 	$('#map').on('click', function(e){
+ 	$(document).on('click', '#map', function(e){
  		var coordinates = getCoordinates(e);
- 		// console.log(coordinates);
  		var newPin = makePinElement(coordinates);
- 		newPin.appendTo($('.container'));
- 		// console.log(newPin);
  	});
- 	
- 	// 
-	// 
+ 	$(document).on('click','.pin', function(e){
+ 		$(this).remove();
+ 	});
+
 });
